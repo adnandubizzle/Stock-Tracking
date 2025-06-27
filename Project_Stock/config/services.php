d9419f2adb7742d03e28364209d233abb5035b58
Adnan <adnan.umar@dubizzle.com>
2025-06-27 19:28:59 +0500

    updated phpunit.xml and fixed other functionalities

--- Project_Stock/config/services.php
+++ Project_Stock/config/services.php
@@ -34,5 +34,10 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+        'clients' => [
+        'bestbuy' => [
+            'key' => env('BEST_BUY_API_KEY'),
+        ],
+    ],
 
 ];
--- Project_Stock/tests/Clients/BestBuyTest.php
+++ Project_Stock/tests/Clients/BestBuyTest.php
@@ -1,25 +1,46 @@
 <?php
+
 namespace Tests\Clients;
 
 use App\Clients\BestBuy;
+use App\Clients\Client;
+use App\Models\Stock;
 use Database\Seeders\RetailerWithProductSeeder;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-Class BestBuyTest extends TestCase
+
+
+//if we use command like vendor/pin/phpunit --exclude-group-api , it will run all tests except the ones marked with this group 
+/** @group api */
+
+
+class BestBuyTest extends TestCase
 {
     use RefreshDatabase;
+
     /** @test */
-    function it_tracks_a_product()
-
+    public function it_tracks_a_product()
     {
-       $this->seed(RetailerWithProductSeeder::class);
-         $stock=tap (Stock::first())->update([
-            'sku'=> '6364253' , 
-            'url' => 'https://www.bestbuy.com/site/nintendo-switch-32gb-console-gray/6364253.p?skuId=6364253',
-         ]);
+        // given i have a product , with stock at best buy
+        // if i use the best buy client to track that stock , it should return an appropriate stock status
 
-         (new BestBuy)
+        // seed the data
+        $this->seed(RetailerWithProductSeeder::class);
+
+        $stock = tap(Stock::first())->update([
+            'sku' => '6470923',
+            // sku is taken from best buy website, this sku is for nintendo switch
+
+            'url' => 'https://www.bestbuy.com/site/switch-oled-model-w-joy-con-nintendo-switch-oled-model/6470923.p?skuId=6470923',
+        ]);
+
+        try {
+            $stockStatus = (new BestBuy)->checkAvailability($stock);
+        } catch (Exception $e) {
+            $this->fail('failed to track API of BB');
+
+        }
 
     }
-
 }
